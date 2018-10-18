8dc6776adbd55c164af8c6dddabea72ad2e5acc8
Mostafa-Nafie <44211916+Mostafa-Nafie@users.noreply.github.com>
2018-10-18 18:51:25 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("hello, world")
-#mostafa alaa
+def search (words,key) :                                        #binary search function
+    first = 0
+    last = len(words)-1
+    while first <= last :
+        mid = (first + last)//2
+        if words[mid] == key :
+            return True
+        elif key < words[mid] :
+            last = mid - 1
+        else :
+            first = mid + 1
+    return False
+
+
+
+with open('Dic.txt', 'r') as reading_dictionary:
+    words_list = reading_dictionary.read().split()        # returns a list to save the words in
+
+word = input().lower()
+
